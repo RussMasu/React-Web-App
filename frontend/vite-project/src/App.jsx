a88698f32dba1e6585842a980d72dfbfbc37c631
RussMasu <masuda3@hawaii.edu>
2025-10-11 13:53:36 -1000

    improved layout, added temple picture to home page

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -5,6 +5,7 @@ import {
     BrowserRouter as Router,
     Routes,
     Route,
+    Link
 } from "react-router";
 import Home from "./pages";
 import Weather from "./pages/weather";
@@ -12,13 +13,13 @@ import Weather from "./pages/weather";
 function App() {
   return (
     <Router>
-      <Stack justifyContent="center" direction="row" spacing={20}>
+      <Stack justifyContent="space-between" direction="row" spacing={20}>
         <h1>RussMasu</h1>
-        <input value="search bar"></input>
+        <Box alignContent="center"><input value="search bar"></input></Box>
       </Stack>
-      <Stack justifyContent="center" direction="row" spacing={20} sx={{bgcolor:"#01579b"}}>
-        <a href="/">Home</a>
-        <a href="/weather">Weather</a>
+      <Stack justifyContent="center" direction="row" spacing={20} sx={{bgcolor:"#01579b", height:"70px"}}>
+        <Button component={Link} to="/" sx={{color:"#ffffff"}}>Home</Button>
+        <Button component={Link} to="/weather"sx={{color:"#ffffff"}}>Weather</Button>
       </Stack>
       <Routes>
           <Route exact path="/" element={<Home />} />
--- frontend/vite-project/src/pages/index.jsx
+++ frontend/vite-project/src/pages/index.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { Divider, Stack, Box } from '@mui/material';
+import picTemple from '../assets/temple.png';
+
 
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <h2>Let's plan your next trip to Hokkaido, Japan!</h2>
+            <p style={{fontSize:"18px"}}>From the Sapporo Snow Festival to the Otaru Aquarium there a plenty of places to visit.  Learn about more about which popular tourist destinations to visit and which places to eat at in this offical guide. </p>
+            <Divider style={{marginLeft:"105px",marginRight:"105px"}}></Divider>
+            <Stack justifyContent="" direction="row" spacing={20}>
+                <Box>
+                    <h1>quicklinks</h1>
+                </Box>
+                <Box>
+                    <figure>
+                    <img src={picTemple} alt='Chureito Pagoda' width="800" height="500"/>
+                    <figcaption>Chureito Pagoda with Mount Fuji.</figcaption>
+                    </figure>
+                    <h3>Title</h3>
+                    <p>Sample Text</p>
+                </Box>
+            </Stack>
         </div>
     );
 };
